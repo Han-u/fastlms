ee676d4ccef08aad1bfa5825226d33386be62fa6
han <chos9715@naver.com>
2022-09-28 21:11:25 +0900

    resposne 객체를 이용한 HTML 작성

---
+++ src/main/java/com/zb/fastlms/MainPage.java
@@ -0,0 +1,44 @@
+package com.zb.fastlms;
+
+// 목적: 주소와 물리적인 파일 매핑하기 위해서
+
+// 주소에 대해서 어디서 매핑하고 누가 매핑하느냐
+// 클래스, 속성, 메소드
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+public class MainPage {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Index Page";
+    }
+
+    // request -> WEB -> SERVER
+    // response -> SERVER -> WEB
+
+    @RequestMapping("/hello")
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+
+        String msg = "<html>" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "</head>" +
+                "<body>" +
+                "<p>hello</p> <p>fastlms website!!!</p>" +
+                "<p>안녕하세용</p>" +
+                "</body>" +
+                "</html>";
+        printWriter.write(msg);
+        printWriter.close();
+    }
+}
